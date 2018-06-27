94b6419206edab0d603109b1293cd25a4c7ecefc
Otavio Pellicano <otavio.pellicano@gmail.com>
2018-06-27 05:53:51 -0300

    estou salvando o log dos clientes conectados e desconectados

--- gerenciaconexao.cpp
+++ gerenciaconexao.cpp
@@ -2,7 +2,8 @@
 
 GerenciaConexao::GerenciaConexao()
 {
-
+    setNomeArqOut("log");
+    setupDir();
 }
 
 GerenciaConexao::~GerenciaConexao()
@@ -10,12 +11,13 @@ GerenciaConexao::~GerenciaConexao()
     mMapNickDescript.clear();
 }
 
-bool GerenciaConexao::addNickname(const QString &nickanme, const qintptr &descript)
+bool GerenciaConexao::addNickname(const QString &nick, const qintptr &descript)
 {
-    if(mMapNickDescript.find(nickanme) != mMapNickDescript.end())
+    if(mMapNickDescript.find(nick) != mMapNickDescript.end())
         return false;
 
-    mMapNickDescript[nickanme] = descript;
+    mMapNickDescript[nick] = descript;
+    salvarLog(CONECTADO, nickname(descript));
     return true;
 
 }
@@ -31,11 +33,90 @@ void GerenciaConexao::rmNickname(const qintptr &descript)
     {
         if(itMap->second == descript)
         {
+            salvarLog(DESCONECTADO, nickname(descript));
             mMapNickDescript.erase(itMap->first);
             return;
         }
-
     }
 
 
 }
+
+QString GerenciaConexao::nickname(const qintptr &descript)
+{
+    for(auto itMap = mMapNickDescript.begin(); itMap != mMapNickDescript.end(); ++itMap)
+    {
+        if(itMap->second == descript)
+        {
+            return itMap->first;
+        }
+    }
+    return QString("");
+}
+
+/**
+ * @brief GerenciaConexao::salvarLog
+ * @param status
+ * @return
+ * Modelo de salvamento
+ * yyyy-MM-dd HH:mm:ss -> cliente status
+ */
+
+void GerenciaConexao::salvarLog(const enumStatus &status, const QString &qstrCliente)
+{
+    QString log = QDateTime::currentDateTime().toString("yyyy-MM-dd HH:mm:ss");
+
+    log = log % " -> " % qstrCliente;
+
+    if(status == CONECTADO)
+    {
+        log = log % " conectado";
+    }
+    else if(status == DESCONECTADO)
+    {
+        log = log % " desconectado";
+    }
+    else
+    {
+        qDebug() << "status invalido!";
+        return;
+    }
+
+    ofstream arq(dirLog().absoluteFilePath(nomeArqOut()).toStdString(), ofstream::app);
+    if(arq.is_open())
+    {
+        arq << log.toStdString() << std::endl;
+        arq.close();
+    }
+    else
+    {
+        qDebug() << "impossivel abrir arquivo de log:" << nomeArqOut();
+        return;
+    }
+
+
+}
+
+
+void GerenciaConexao::setupDir()
+{
+    mDirLog.mkdir("log");
+    mDirLog.cd("log");
+}
+
+void GerenciaConexao::setNomeArqOut(const QString &nomeArqOut)
+{
+    mNomeArqOut = nomeArqOut % "-" % QDate::currentDate().toString("yyyy-MM-dd") % ".txt";
+}
+
+
+QString GerenciaConexao::nomeArqOut() const
+{
+    return mNomeArqOut;
+}
+
+QDir GerenciaConexao::dirLog() const
+{
+    return mDirLog;
+}
+
--- gerenciaconexao.h
+++ gerenciaconexao.h
@@ -2,23 +2,46 @@
 #define GERENCIACONEXAO_H
 
 #include <QString>
+#include <QStringBuilder>
 #include <map>
+#include <fstream>
+#include <QDir>
+#include <QDateTime>
+#include <QDebug>
+
 
 using std::map;
-
+using std::ofstream;
 class GerenciaConexao
 {
 public:
     GerenciaConexao();
     virtual ~GerenciaConexao();
 
-    bool addNickname(const QString &nickanme, const qintptr &descript);
+    bool addNickname(const QString &nick, const qintptr &descript);
     void rmNickname(const QString &nickname);
     void rmNickname(const qintptr &descript);
+    QString nickname(const qintptr &descript);
+
+    void setNomeArqOut(const QString &nomeArqOut);
+
+private:
+    QDir dirLog() const;
+
+    QString nomeArqOut() const;
+
+    enum enumStatus{CONECTADO, DESCONECTADO};
+
+    void salvarLog(const enumStatus &status, const QString &qstrCliente);
+
+    void setupDir();
 
 
 private:
     map<QString, qintptr> mMapNickDescript;
+    QDir mDirLog;
+    QString mNomeArqOut;
+
 };
 
 #endif // GERENCIACONEXAO_H
